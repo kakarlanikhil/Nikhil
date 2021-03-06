cbbc7a8aafe34c2ef0eb0daff794f10b868c3e54
kakarlanikhil <80156728+kakarlanikhil@users.noreply.github.com>
2021-03-06 17:18:59 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60436afb465b2a001e0ecccf",
+				"60436afb465b2a001e0eccd1"
+			]
+		},
+		"60436afb465b2a001e0ecccf": {
+			"id": "60436afb465b2a001e0ecccf",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60436afb465b2a001e0eccd1": {
+			"id": "60436afb465b2a001e0eccd1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
